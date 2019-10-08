74c0b92f425d2f61f5a2f55bfbad356009304d90
Mykola Maslov <mykolamaslov@mykolas-mbp.home>
2019-10-08 16:33:44 -0400

    Added the cart detail component, added the cart URL  to the routing configuration

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,9 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+// Logger with default options
+import logger from "redux-logger";
 
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
 
 export const SportsStoreDataStore = createStore(
-  CommonReducer(ShopReducer, CartReducer)
+  CommonReducer(ShopReducer, CartReducer),
+  applyMiddleware(logger)
 );
